c047899ded8dd5e7b9ebe0b580c060136102e1f0
Ogün Gültekin <ogun.gultekin@outlook.com.tr>
2024-02-25 19:04:06 +0300

    test

---
+++ src/test/java/com/apiteknik/DevOpsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiteknik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
